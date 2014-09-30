9cb30d8cda16af600b83086b2d01b8a6bc65edd3
A. Varnin <fenixk19@mail.ru>
2014-09-30 11:34:58 +0400

    Program initial version

--- README.md
+++ README.md
@@ -1,4 +1,38 @@
 vk2diaspora
 ===========
 
-Simple script to transfer entries from vk.com public page to diaspora network
+Simple script to transfer entries from vk.com public pages to diaspora* network
+
+Prerequisites
+-------------
+
+  1. Ruby 1.9.3
+  2. Ruby Gem 'vkontakte_api' installed (tested with v. 1.4)
+  3. Cliaspora [1] (tested with v. 0.1.9)
+  
+Usage
+-----
+
+This script uses Cliaspora [1] to make posts to d* network. vk2diaspora script assumes, that you have set up session with Cliaspora in the following way:
+
+$ cliaspora session new yourname@diasp.eu
+
+It will ask you of your password and will save session cookie.
+
+Now you could use vk2diaspora. Command syntax is following:
+```
+$ vk2diaspora <vk page short address> <diaspora account> <diaspora aspect> [additional tags]
+```
+
+For example, you could run the following to transfer entries from https://vk.com/anons_rev to your diaspora account yourname@diasp.eu "test" aspect:
+
+$ vkdiaspora anons_rev yourname@diasp.eu test "#vk2diaspora"
+
+Additional tags will be appended as a last line of your post. 
+
+Use the word "public" for aspect to post your entries available for view by everyone.
+
+The script will create directory ~/vk2diaspora.timestamps to store timestamps of last posted entry (taken from vk.com API answer). So if you rerun script, it will transfer only that entries, which are newer than this timestamp. It makes possible to continue post transfer as the parent public page updates.
+
+[1] https://freeshell.de//~mk/projects/cliaspora.html
+
---
+++ vk2diaspora.rb
@@ -0,0 +1,107 @@
+#!/usr/bin/ruby
+# encoding: utf-8
+
+#
+#    Copyright 2014 A. Varnin
+#    
+#    This program is free software: you can redistribute it and/or modify
+#    it under the terms of the GNU General Public License as published by
+#    the Free Software Foundation, either version 3 of the License, or
+#    (at your option) any later version.
+#
+#    This program is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#    GNU General Public License for more details.
+#
+#    You should have received a copy of the GNU General Public License
+#    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+require 'vkontakte_api'
+require 'open3'
+
+cliaspora_path="cliaspora"
+timestamps_dir=File.join(Dir.home, "vk2diaspora.timestamps")
+
+page_domain=ARGV[0]
+account=ARGV[1]
+aspect=ARGV[2]
+tags=ARGV[3]
+
+if page_domain == nil or account == nil or aspect == nil
+	puts "Usage vk2diaspora <vk page short address> <diaspora account> <diaspora aspect> [additional tags]"
+	exit
+end
+
+begin
+	Dir.mkdir(timestamps_dir, 0700)
+rescue Errno::EEXIST
+end
+
+timestamp_file = File.join(timestamps_dir, account)
+
+@vk = VkontakteApi::Client.new
+
+
+entries = @vk.wall.get(domain: page_domain).reverse
+
+timestamp = 0
+
+if File.exists?(timestamp_file)
+	File.open(timestamp_file) { |file|
+	 	timestamp = file.gets
+	}
+end
+
+
+i=0
+
+while timestamp.to_i >= entries[i].date
+	i += 1
+	if i == entries.length - 1
+		puts "No new entries"
+		exit
+	end
+end
+
+while i < (entries.length - 1)
+
+	url = ""
+	post = entries[i].text
+	timestamp = entries[i].date
+	entries[i].attachments.each {|a| 
+		if a.type == "photo"
+			url = a.photo.src_xbig
+		end
+	}
+	if url.length > 0
+		post += "\n![image](" + url + ")"
+	end
+	post = post.gsub("http://vk.com", "https://vk.com")
+	post = post.gsub("<br>", "\n")
+	post+="\n";
+	if tags != nil
+		post+=tags+"\n"
+	end
+
+	puts post
+	w, wt = Open3.pipeline_w (cliaspora_path + " -a " + account + " post " + aspect) 
+	w.puts post
+	w.close
+
+	status = wt[0].value.to_i
+	puts "Status: "
+	puts status
+
+	if status == 0
+		File.open(timestamp_file, "w") { |file|
+			file.puts timestamp
+		}
+	else
+		puts "Something went wrong. Exiting."
+		exit
+	end
+
+	i += 1
+end
+
